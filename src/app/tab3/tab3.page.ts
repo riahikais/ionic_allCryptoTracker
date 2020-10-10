2d25a0047ba3f5bde28c930801227a1de9180612
riahikais <riahikais@yahoo.com>
2020-10-10 20:12:16 +0100

    Adding eth, litecoin and xrp

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CryptoCoin} from '../models/crypto-coin';
+import {CryptoCoinService} from '../services/crypto-coin.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  selectedFiat = 'USD';
+  fiats = ['USD', 'EUR', 'GBP', 'JPY'];
+  bitcoin$: CryptoCoin;
+  constructor(private coinProvider: CryptoCoinService) {}
+
+  async ngOnInit(){
+    this.changeFiat();
+    console.log('bit', this.bitcoin$);
+  }
+
+  callService(){
+    return this.coinProvider.getCoinInfo(this.selectedFiat, 'ethereum').toPromise();
+  }
+
+  async changeFiat(){
+    let d = await this.callService();
+    this.bitcoin$ = d[0];
+  }
+
 
 }
